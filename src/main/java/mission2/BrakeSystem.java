d9a6470df1faf0fc466bb9d97c90f967d1d73fc7
jaehyung <jae637@naver.com>
2025-07-24 13:40:36 +0900

    [D2] 각 옵션별 유효성 검사 로직 추가

--- src/main/java/mission2/BrakeSystem.java
+++ src/main/java/mission2/BrakeSystem.java
@@ -13,4 +13,13 @@ public class BrakeSystem implements BrakeSystemInterface{
         String name = a == 1 ? "MANDO" : a == 2 ? "CONTINENTAL" : "BOSCH";
         System.out.printf("%s 제동장치를 선택하셨습니다.\n", name);
     }
+
+    @Override
+    public boolean validCheck(int ans) {
+        if (ans < 0 || ans > 3) {
+            System.out.println("ERROR :: 제동장치는 1 ~ 3 범위만 선택 가능");
+            return false;
+        }
+        return true;
+    }
 }
